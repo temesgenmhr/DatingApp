a4664bf01966bb167bbb6c7e3e58c13f1ba7d815
Temesgen Mihiretu <tmsmhrabebe@outlook.com>
2023-01-29 18:13:55 +0300

    end of sec11

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,37 +1,110 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
-/*   @Input() usersFromHomeComponent: any;  */
+  /*   @Input() usersFromHomeComponent: any;  */
   @Output() cancelRegister = new EventEmitter();
-  model: any = {}
+  registerForm: FormGroup = new FormGroup({});
+  maxDate: Date = new Date();
+  validationErrors: string[] | undefined;
 
-  constructor(private accountService: AccountService, private toastr: ToastrService) { }
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.initializeForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
 
-  register(){
-   this.accountService.register(this.model).subscribe({
-    next: response =>{
-      console.log(response);
-      this.cancel();
-    },
-    error: error => {
-      this.toastr.error(error.error);
-        console.log(error);
-    }
-   })
+  initializeForm() {
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: [
+        '',
+        [Validators.required, Validators.minLength(4), Validators.maxLength(8)],
+      ],
+      confirmPassword: ['', [Validators.required, this.matchValue('password')]],
+    });
+
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () =>
+        this.registerForm.controls['confirmPassword'].updateValueAndValidity(),
+    });
+  }
+  /* initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(8),
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        this.matchValue('password'),
+      ]),
+    });
+
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () =>
+        this.registerForm.controls['confirmPassword'].updateValueAndValidity(),
+    });
+  } */
+
+  matchValue(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value
+        ? null
+        : { notMatching: true };
+    };
+  }
+  register() {
+    const dob = this.getDateOnly(this.registerForm.controls['dateOfBirth'].value);
+    const values = {...this.registerForm.value, dateOfBirth:dob};
+
+    this.accountService.register(values).subscribe({
+      next: (response) => {
+        console.log(response);
+        // this.cancel();
+        this.router.navigateByUrl('/members');
+      },
+      error: (error) => {
+       this.validationErrors = error;
+      },
+    });
   }
 
-  cancel(){
+  cancel() {
     this.cancelRegister.emit(false);
+  }
+
+  private getDateOnly(dob: Date | undefined){
+    if(!dob) return;
+    let theDob = new Date(dob);
+    return new Date(theDob.setMinutes(theDob.getMinutes() - theDob.getTimezoneOffset())).toISOString().slice(0, 10);
   }
 }
